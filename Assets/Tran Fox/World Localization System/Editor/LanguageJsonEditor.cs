b593c005158cf038d10da7275dd214dc513f47b1
Tran Fox <22718078+Tran-Foxxo@users.noreply.github.com>
2024-07-11 13:39:22 -0400

    Adding + Deleting Keys
    Also some slight refactoring

--- Assets/Tran Fox/World Localization System/Editor/LanguageJsonEditor.cs
+++ Assets/Tran Fox/World Localization System/Editor/LanguageJsonEditor.cs
@@ -71,51 +71,92 @@ namespace TranFox.LocalizationSystem
             EditorGUILayout.Space(10);
 
             // Get keys
-            List<string> keys = new List<string>();
+            List<string> stringKeys = new List<string>();
             var dictKeys = _LoadedLanguage.GetKeys();
             for (int i = 0; i < dictKeys.Count; i++)
             {
                 var dictKey = dictKeys[i];
 
                 if (dictKey.TokenType == TokenType.String)
-                    keys.Add(dictKey.String);
+                    stringKeys.Add(dictKey.String);
             }
 
             _KeySearch = EditorGUILayout.TextField("Key Search: ", _KeySearch);
 
             List<string> keysAfterSearch = new List<string>();
-            foreach (string key in keys)
+            foreach (string key in stringKeys)
             {
                 if (key.Contains(_KeySearch, System.StringComparison.CurrentCultureIgnoreCase))
                     if (_LoadedLanguage[key].TokenType == TokenType.String)
                         keysAfterSearch.Add(key);
             }
 
-            if (keysAfterSearch.Count == 0)
-            {
-                if (GUILayout.Button($"No keys found, click to add \"{_KeySearch}\""))
-                    _LoadedLanguage.Add(_KeySearch, "");
-                return;
-            }
-
+            // Key selection dropdown
             _SelectedKeySearchIndex = EditorGUILayout.Popup("Selected Key: ", _SelectedKeySearchIndex, keysAfterSearch.ToArray());
-
             if (_SelectedKeySearchIndex >= keysAfterSearch.Count)
             {
-                _SelectedKeySearchIndex = 0;
+                _SelectedKeySearchIndex = 0; // Set to 0 if over, happens when changing search
             }
-
-            string currentKey = keysAfterSearch[_SelectedKeySearchIndex];
 
             EditorGUILayout.Space(10);
 
+            // Add Key
+            if (!stringKeys.Contains(_KeySearch) && _KeySearch != "")
+            {
+                if (GUILayout.Button($"Add Key \"{_KeySearch}\""))
+                    _LoadedLanguage.Add(_KeySearch, "");
+            }
+            if (keysAfterSearch.Count == 0) { return; }
+            
+            // Get selected key
+            string currentKey = keysAfterSearch[_SelectedKeySearchIndex];
+
+            // Key Deletion
+            if (GUILayout.Button($"Remove Key \"{currentKey}\""))
+            {
+                // This code spacing looks REALLY ugly but it's readable enough...
+                if (EditorUtility.DisplayDialog(
+                    "[LanguageJsonEditor] Key Deletion",
+                    $"Are you sure you want to delete \"{currentKey}\"?",
+                    "Yes",
+                    "No")
+                    ) 
+                { 
+                    _LoadedLanguage.Remove(currentKey); 
+                }
+            }
+
+            EditorGUILayout.Space(10);
+
+            // Edtinging box
             GUILayout.Label($"KEY \"{currentKey}\":", EditorStyles.boldLabel);
             _LoadedLanguage[currentKey] = EditorGUILayout.TextArea(_LoadedLanguage[currentKey].String);
 
+            // Saving
             if (GUILayout.Button("Save Language"))
             {
-                var outToken = new DataToken();
-                bool worked = VRCJson.TrySerializeToJson(_LoadedLanguage, JsonExportType.Beautify, out outToken);
+                // So for SOME reason, deleting a key leaves a blank "null" value and keeps the key.
+                // So instead we will manually only save string keys & values.
+                DataDictionary fixedDictionary = new DataDictionary();
+                var keys = _LoadedLanguage.GetKeys();
+                var values = _LoadedLanguage.GetValues();
+                for (int i = 0; i < _LoadedLanguage.Count; i++)
+                {
+                    if (keys[i].TokenType == TokenType.String && values[i].TokenType == TokenType.String)
+                    {
+                        string keyString = keys[i].String;
+                        string valueString = values[i].String;
+
+                        // Remove \r's this may cause unexpected behaviour with TMP actually supporting them
+                        // If you really need \r for some reason just comment out this line I guess.
+                        valueString = valueString.Replace("\r", "");
+
+                        fixedDictionary.Add(keyString, valueString);
+                    }
+                }
+
+                var outToken = new DataToken(); 
+                bool worked = VRCJson.TrySerializeToJson(fixedDictionary, JsonExportType.Beautify, out outToken);
 
                 if (!worked) { Debug.LogError("[LanguageJsonEditor] UNABLE TO SAVE FOR SOME REASON???"); return; }
 
